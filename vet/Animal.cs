0dea7f72ec7e10998024d9b1b7f0348b3bbd0101
soltyDude <139552638+soltyDude@users.noreply.github.com>
2024-04-12 00:27:46 +0300

    vet rewrite

--- vet/Animal.cs
+++
@@ -1,8 +0,0 @@
-public class Animal 
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Category { get; set; } // e.g., Dog, Cat
-    public double Weight { get; set; }
-    public string FurColor { get; set; }
-}
--- vet/DataStore.cs
+++
@@ -1,5 +0,0 @@
-public static class DataStore 
-{
-    public static List<Animal> Animals = new List<Animal>();
-    public static List<Visit> Visits = new List<Visit>();
-}
